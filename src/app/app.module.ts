812d07e991878db8abe06f7066b13982f86b98c4
Reza Fathollahi <reza.fathollahi96@gmail.com>
2023-09-15 16:25:50 +0430

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule , CommonModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
